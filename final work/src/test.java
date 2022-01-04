808718e38d47ff18fb326a482df3d718da2c5405
oldpincon <ls0968798609@gmail.com>
2022-01-04 14:16:04 +0800

    version 2 add some comment

--- final work/src/test.java
+++ final work/src/test.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://hk.investing.com/crypto/currencies").get();
+        Document doc = Jsoup.connect("https://hk.investing.com/crypto/currencies").get(); //我們爬的網站
         System.out.println("漲跌劇烈提示器");
         System.out.println("-------------------------------------------");
         System.out.println("今日警示");
 
         System.out.println("-------------------------------------------");
-        Element item = doc.select("#fullColumn h1 ").first();
-        System.out.println(item.text());
-        Element table = doc.select("table").last();
+        Element title = doc.select("#fullColumn h1 ").first(); //此部分的標題
+        System.out.println(title.text());
+        Element table = doc.select("table").last(); //所有貨幣的資料
         System.out.print(table.text());
 
     }
