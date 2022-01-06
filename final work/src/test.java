290aa95aefacfa554e71c6f06955f75e785eb047
c110181130 <peixianwu101@gamil.com>
2022-01-06 21:41:51 +0800

    1/6 我真的太難了 進度嚴重落後 還有很多問題 林國揚快點出現 不要躲了好嗎 有問題就要出來解決 躲的了一時躲不了一世 (Ò 皿 Ó ╬)

--- final work/src/test.java
+++ final work/src/test.java
@@ -17,21 +17,13 @@ public class test {
 
 
         System.out.println("-------------------------------------------");
-        System.out.println("今日警示 :" + "\n"); // \n是換行
+        System.out.println("今日警示 :"); // \n是換行
         /////// 打咚咚
         System.out.println("-------------------------------------------");
 
         Element title = doc.select("#fullColumn h1 ").first(); //此部分的標題 顯示所有加密貨幣 標題
-        System.out.println(title.text());
+        System.out.println(title.text()+":\n");
 
-        //Element table = doc.select("table").last(); //所有貨幣的資料
-        //System.out.print(table.text());
-
-        System.out.println("-----------------------------------------------------------------------------");
-        Element table = doc.select("thead").last(); //所有貨幣的資料表
-        System.out.print(table.text());
-
-        System.out.println("\n" + "-----------------------------------------------------------------------------");// \n是換行
         ////////////////////////// 給他們一點自己的空間
         for (int i=0 ; i<10 ; i++) {
             String table1 = doc.getElementsByClass("left noWrap elp symb js-currency-symbol").get(i).text();
@@ -40,9 +32,10 @@ public class test {
             String table4 = doc.getElementsByClass("js-market-cap").get(i).text();
             String table5 = doc.getElementsByClass("js-24h-volume").get(i).text();
             String table6 = doc.getElementsByClass("js-total-vol").get(i).text();
-            //String table7 = doc.getElementsByClass("js-currency-change-24h pid-1057391-pcp redFont").get(7).text();
+            //String table7 = doc.getElementsByClass("1057391").get(0).text();
             //String table7 = doc.select("#fullColumn > div:nth-child(9) > table > tbody > tr:nth-child(1) > td.js-currency-change-24h.pid-1057391-pcp.redFont").get(0).text();
             String table8 = doc.getElementsByClass("js-currency-change-7d redFont").get(i).text();
+
 
             System.out.println((i+1)+"."+" 名稱: " + table2 + " 代號: " + table1 + " 價格 (USD): " + table3 + " 市值: " + table4 + "\n    成交量(24小時): " + table5 + " 市場份額: " + table6 + " 升跌率 (24小時): "  + " 升跌率(7日): " + table8+ "\n");
             // table 7 一直錯誤 我真的沒辦法 煩死人了!
