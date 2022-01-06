afd80f2c41c6cf0853f86cda35b6fce5978f52ab
c110181130 <93572202+c110181130@users.noreply.github.com>
2022-01-06 18:02:50 +0800

    Add files via upload

--- test.java
+++ test.java
@@ -1,8 +1,11 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.IOException ;
+//import javax.lang.model.util.ElementScanner6;
+import java.io.IOException;
+
 
 public class test {
     public static void main(String[] args) throws IOException {
@@ -30,17 +33,17 @@ public class test {
         System.out.print(table.text());
         System.out.println("\n" + "-----------------------------------------------------------------------------");// \n是換行
 
-        Element table2 = doc.select("tbody").last(); //所有貨幣的資料 //全部排成一行 不能換行
-        System.out.print(table2.text());
+        //Element table2 = doc.select("tbody").last(); //所有貨幣的資料 //全部排成一行 不能換行
+        //System.out.print(table2.text());
 
-       // Elements items = doc.getElementsByClass("tr class");
-      //  for (Element item : items) {
-      //      String a  = item.getElementsByClass("td").get(0).getElementsByTag("rank icon").get(0).text();
-      //      String b  = item.getElementsByClass("td").get(0).getElementsByTag("left noWrap elp symb js-currency-symbol").get(0).text();
-      //      System.out.println(a);
-      //      System.out.println(b);
-      //      //36-40 我找不到錯 也不知道這樣打是不是正確的 要瘋掉了 有時候還會跳第9行錯
-      //  }
+       Elements items = doc.getElementsByClass("tr class");
+       for (Element item : items) {
+           String a  = item.getElementsByClass("tr").get(0).getElementsByTag("rank icon").get(0).text();
+           String b  = item.getElementsByClass("tr").get(0).getElementsByTag("left noWrap elp symb js-currency-symbol").get(0).text();
+           System.out.println(a);
+           System.out.println(b);
+           //36-40 不知道這樣打是不是正確的 也不知道為甚麼String後面只能接一個字母 不能是單字 要瘋掉了拉乾 而且顯示不出來吼
+       }
 
     }
     }
