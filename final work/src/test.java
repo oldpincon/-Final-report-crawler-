dc925de65b2bd895d3b0cb7a1312510b6eafee7f
c110181130 <peixianwu101@gamil.com>
2022-01-10 19:03:26 +0800

    1/10 我是天才吧!!! 沒有啦宏宇才是 靠北喔select下來的是文字 所以不能比大小 所以做不了警示 我真的會瘋掉 ==

--- final work/src/test.java
+++ final work/src/test.java
@@ -10,25 +10,14 @@ public class test {
             Document doc1 = Jsoup.connect("https://hk.investing.com/crypto/").get();
 
             System.out.println("漲跌劇烈提示器");
-            /////// 打咚咚
+
+           // System.out.println("-------------------------------------------");
+           // System.out.println("今日警示 :"); // \n是換行
 
             System.out.println("-------------------------------------------");
-            System.out.println("今日警示 :"); // \n是換行
-            // for (int a = 0; a < 10; a++) {
-
-            //     String table9 = doc.getElementsByClass("js-currency-change-7d redFont").get(a).text();
-            //     String table2 = doc.getElementsByClass("left bold elp name cryptoName first js-currency-name").get(a).text();
-            //     String c = table9;
-            //     if (c > 0.91) {
-            //         System.out.println("12");
-            //     } else {
-            //         System.out.println("21");
-            //     }
-            // }
-            System.out.println("-------------------------------------------");
+            System.out.println("前10個幣值資料:");
             int j = 0;
             for (int i = 1; i <= 10; i++) {
-
                 String table2 = doc.getElementsByClass("left bold elp name cryptoName first js-currency-name").get(j).text();
                 String table3 = doc.getElementsByClass("left noWrap elp symb js-currency-symbol").get(j).text();
                 String table4 = doc.getElementsByClass("price js-currency-price").get(j).text();
@@ -38,16 +27,30 @@ public class test {
                 String table9 = doc.getElementsByClass("js-currency-change-7d redFont").get(j).text();
                 Elements updo = doc1.select("#fullColumn > div:nth-child(9) > table > tbody > tr:nth-child(" + i + ")");
                 Element table10 = updo.get(0);
+                //String table11 = doc.select("#fullColumn > div:nth-child(14) > table > tbody > tr:nth-child("+i+") > td.js-currency-change-24h.greenFont.pid-"+ table10.attr("a") +"-pcp").get(i).text();
+                //String table12 = doc.select("#fullColumn > div:nth-child(14) > table > tbody > tr:nth-child("+i+") > td.js-currency-change-24h.redFont.pid-"+ table10.attr("a") +"-pcp").get(i).text();
 
                 if(doc.select("#fullColumn > div:nth-child(14) > table > tbody > tr:nth-child("+i+") > td.js-currency-change-24h.greenFont.pid-"+ table10.attr("i") +"-pcp").size() != 0){
                     String table8 = doc.getElementsByClass("js-currency-change-24h greenFont pid-" + table10.attr("i") +"-pcp").get(0).text();
-                    System.out.println(i  + "." + "名稱: " + table2 + " 代號: " + table3 + " 價格 (USD): " + table4 + " 市值: " + table5 + "\n   成交量(24小時): " + table6 + " 市場份額: " + table7 + " 升跌率 (24小時): " +table8 + " 升跌率(7日): " + table9 + "\n");
-
-                }else{
-                    String table8 = doc.getElementsByClass("js-currency-change-24h redFont pid-" + table10.attr("i") +"-pcp").get(0).text();
-                    System.out.println(i + "." + "名稱: " + table2 + " 代號: " + table3 + " 價格 (USD): " + table4 + " 市值: " + table5 + "\n   成交量(24小時): " + table6 + " 市場份額: " + table7 + " 升跌率 (24小時): " +table8 + " 升跌率(7日): " + table9 + "\n");
+                    System.out.println(i  + "." + "名稱: " + table2 + " 代號: " + table3 + " 價格 (USD): " + table4 + " 市值: " + table5 + "\n   成交量(24小時): " + table6 + "   市場份額: " + table7 + " 升跌率 (24小時): " +table8 + " 升跌率(7日): " + table9 + "\n");
                 }
-                j++;
+                else {
+                    String table8 = doc.getElementsByClass("js-currency-change-24h redFont pid-" + table10.attr("i") +"-pcp").get(0).text();
+                    System.out.println(i + "." + "名稱: " + table2 + " 代號: " + table3 + " 價格 (USD): " + table4 + " 市值: " + table5 + "\n  成交量(24小時): " + table6 + "     市場份額: " + table7 + " 升跌率 (24小時): " +table8 + " 升跌率(7日): " + table9 + "\n");
+                }
+
+               //double table13 = 1.5 ;
+               //double table14 =0.2;
+               //if (table11 > table13){
+               //    System.out.println("今日警示 :"+table2+ "的漲幅為"+table11);
+               //}
+               //else if (table12 <table14){
+               //    System.out.println("今日警示 :"+table2+ "的漲幅為"+table12);
+               //}
+               //else {
+               //    System.out.println("\n");
+               //}
+               //j++;
             }
         }
         catch (Exception e){
