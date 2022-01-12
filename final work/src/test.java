06e676ac08ee26951a98a51c6921a0e5d7882a57
oldpincon <ls0968798609@gmail.com>
2022-01-12 22:53:45 +0800

    bonus homework

--- final work/src/test.java
+++ final work/src/test.java
@@ -1,4 +1,4 @@
-import com.sun.source.tree.ContinueTree;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,55 +13,49 @@ public class test {
             System.out.println("漲跌劇烈提示器");//主題
 
             System.out.println("-------------------------------------------");
-            System.out.println("今日警示 :");//寫不出來== 爬下來的是text檔，目前轉不成integer @_@
-         //   for (int i = 1; i <= 10; i++) {
-         //       if(doc.select("#fullColumn > div:nth-child(14) > table > tbody > tr:nth-child("+i+") > td.js-currency-change-24h.greenFont.pid-").size() != 0){
-         //           String tfc = doc.getElementsByClass("js-currency-change-24h greenFont pid-" ).get(0).text();
-         //           tfc = tfc.replaceAll("[^\\d]","");
-         //           double tfcU = Double.parseDouble(tfc);
-         //           if (tfcU > 10)
-         //           System.out.println(tfcU);
-         //           break;
-         //       }
-         //       else {
-         //           String tfc = doc.getElementsByClass("js-currency-change-24h redFont pid-" ).get(0).text();
-         //           tfc = tfc.replaceAll("[^\\d]","");
-         //           double tfcD = Double.parseDouble(tfc);
-         //           if (tfcD < -10)
-         //           System.out.println(tfcD);
-         //           continue;
-         //       }
-         //   }
+            System.out.println("今日警示 :");//寫不出來==
+
+        //   for (int i = 1; i <= 10; i++) {
+        //       Elements updo = doc1.select("#fullColumn > div:nth-child(9) > table > tbody > tr:nth-child(" + i + ")"); //貨幣分別有不同i值,所以要另外抓24hr升跌率
+        //       Element table10 = updo.get(0); // 抓<tr i >整串原始碼
+        //       if(doc.select("#fullColumn > div:nth-child(14) > table > tbody > tr:nth-child("+i+") > td.js-currency-change-24h.greenFont.pid-"+ table10.attr("i") +"-pcp").size() != 0){
+        //           String tfc = doc.getElementsByClass("js-currency-change-24h greenFont pid-" + table10.attr("i") +"-pcp" ).get(i).text();
+        //           tfc = tfc.replaceAll("[^\\d]","");
+        //           double tfcU = Double.parseDouble(tfc);
+        //           if (tfcU > 10)
+        //           System.out.println(tfcU);
+        //       }
+        //       else {
+        //           String tfc = doc.getElementsByClass("js-currency-change-24h redFont pid-" + table10.attr("i") +"-pcp").get(i).text();
+        //           tfc = tfc.replaceAll("[^\\d]","");
+        //           double tfcD = Double.parseDouble(tfc);
+        //           if (tfcD < -10)
+        //           System.out.println(tfcD);
+        //       }
+        //   }
 
 
             System.out.println("-------------------------------------------");
             System.out.println("前10個幣值資料:");//不想取太多個，太後面那些也沒用，如果想看更多請付費解鎖，沒啦!想看我還是能改^^
-            int j = 0;
-            for (int i = 1; i <= 10; i++) {
-                String table2 = doc.getElementsByClass("left bold elp name cryptoName first js-currency-name").get(j).text(); //抓名稱
-                String table3 = doc.getElementsByClass("left noWrap elp symb js-currency-symbol").get(j).text(); //抓代號
-                String table4 = doc.getElementsByClass("price js-currency-price").get(j).text(); //抓價格
-                String table5 = doc.getElementsByClass("js-market-cap").get(j).text(); //抓市值
-                String table6 = doc.getElementsByClass("js-24h-volume").get(j).text(); //抓交易量
-                String table7 = doc.getElementsByClass("js-total-vol").get(j).text(); //ˊ抓市場交易份額
-                String table9 = doc.getElementsByClass("js-currency-change-7d redFont").get(j).text(); //七日內的升跌率
-                Elements updo = doc1.select("#fullColumn > div:nth-child(9) > table > tbody > tr:nth-child(" + i + ")"); //貨幣分別有不同i值,所以要另外抓24hr升跌率
-                Element table10 = updo.get(0); // 抓<tr i >整串原始碼
-
-                if(doc.select("#fullColumn > div:nth-child(14) > table > tbody > tr:nth-child("+i+") > td.js-currency-change-24h.greenFont.pid-"+ table10.attr("i") +"-pcp").size() != 0){
-                    //如果24小時的升率字串數字大小不等於0 則顯示
-                    String tfc = doc.getElementsByClass("js-currency-change-24h greenFont pid-" + table10.attr("i") +"-pcp").get(0).text();
-                    System.out.println(i  + "." + "名稱: " + table2 + " 代號: " + table3 + " 價格 (USD): " + table4 + " 市值: " + table5 + "\n   成交量(24小時): " + table6 + "   市場份額: " + table7 + " 升跌率 (24小時): " +tfc + " 升跌率(7日): " + table9 + "\n");
-                    //tfcU = 24hrㄉ升率，tf = twenty-four 、 c = change
+            for(int a=0; a<10 ; a++){
+                String table100 = doc.getElementsByTag("tr").get(0).getElementsByTag("th").get(a).text();
+                if(a<10){
+                    System.out.printf("%-10s",table100);
                 }
-                else {
-                    //如果24小時的跌率字串數字大小不等於0 則顯示
-                    String tfc = doc.getElementsByClass("js-currency-change-24h redFont pid-" + table10.attr("i") +"-pcp").get(0).text();
-                    System.out.println(i + "." + "名稱: " + table2 + " 代號: " + table3 + " 價格 (USD): " + table4 + " 市值: " + table5 + "\n  成交量(24小時): " + table6 + "     市場份額: " + table7 + " 升跌率 (24小時): " +tfc + " 升跌率(7日): " + table9 + "\n");
-                    //tfcD = 24hrㄉ升率，tf = twenty-four 、 c = change
+                else{
+                    System.out.printf("%-10s",table100,"%-10s");
                 }
-                j++;
             }
+
+            System.out.println("\n");
+            for(int b=1; b<=10 ; b++) {
+                System.out.println("\n");
+                for (int c = 0; c < 10; c++) {
+                    String table1000 = doc.getElementsByTag("tr").get(b).getElementsByTag("td").get(c).text();
+                    System.out.printf("%-11s",table1000,"%-11s");
+                }
+            }
+            System.out.println("\n");
         }
         catch (Exception e){
             System.out.println("error" + e);
